44ebe739834f1fc8cac1abb174e8e0e280e810a7
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-11 12:52:40 -0400

    vd

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-const element = (
-  <>
-    <p>123</p>
-    <ul>
-      <li>
-        <a href="">a</a>
-      </li>
-      <li>
-        <a href="">b</a>
-      </li>
-      <li>
-        <a href="">c</a>
-      </li>
-    </ul>
-  </>
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-ReactDOM.createRoot(document.getElementById('root')).render(element);
+function renderElement() {
+  const element = (
+    <div className="post">
+      <h1>My First Blog Post</h1>
+      <div>Author: Mark Twain</div>
+      <div>Published: {new Date().toLocaleTimeString()}</div>
+      <p>
+        I am new to blogging and this is my first post. You should expect a lot
+        of great things from me.
+      </p>
+    </div>
+  );
+
+  root.render(element);
+}
+setInterval(renderElement, 1000);
