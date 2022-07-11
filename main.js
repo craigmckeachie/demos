7351654aa7a72b1cdbce22b4c4500ef599de8902
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-11 12:34:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const element = (
+  <>
+    <p>123</p>
+    <ul>
+      <li>
+        <a href="">a</a>
+      </li>
+      <li>
+        <a href="">b</a>
+      </li>
+      <li>
+        <a href="">c</a>
+      </li>
+    </ul>
+  </>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
