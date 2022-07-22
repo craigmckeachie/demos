ff22e2daa13a9468113925eeef49272bdab89328
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-22 11:37:22 -0400

    update serve version

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-function renderElement() {
-  const element = (
-    <div className="post">
-      <h1>My First Blog Post</h1>
-      <div>Author: Mark Twain</div>
-      <div>Published: {new Date().toLocaleTimeString()}</div>
-      <p>
-        I am new to blogging and this is my first post. You should expect a lot
-        of great things from me.
-      </p>
-    </div>
-  );
-
-  root.render(element);
+function HelloWorld() {
+  return <div className="container">Hello World</div>;
 }
-setInterval(renderElement, 1000);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HelloWorld />);
